8258bd83ea4e5867d8c26ef3637db81133ac70b3
Aleksey Gorokhov <gorokhov-dev@yandex.ru>
2020-05-13 11:31:06 +0500

    fix: storyshots

--- app/components/currency-card/currency-card.story.tsx
+++ app/components/currency-card/currency-card.story.tsx
@@ -3,14 +3,14 @@ import { storiesOf } from "@storybook/react-native"
 import { StoryScreen, Story, UseCase } from "../../../storybook/views"
 import { CurrencyCard } from "./currency-card"
 
-declare var module
+declare let module
 
 storiesOf("CurrencyCard", module)
   .addDecorator(fn => <StoryScreen>{fn()}</StoryScreen>)
   .add("Style Presets", () => (
     <Story>
       <UseCase text="Primary" usage="The primary.">
-        <CurrencyCard text="CurrencyCard" />
+        <CurrencyCard color={[0.5, 0.5, 0.5]} text="test" />
       </UseCase>
     </Story>
   ))
